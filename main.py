42e821f5cadc57b53577f446a33b101d7dc0a889
Raviteja Inapala <inapala27@gmail.com>
2024-03-08 14:22:56 +0100

    removed delete method and removed instruction code for database creation

--- main.py
+++ main.py
@@ -32,16 +32,15 @@ Base_model=declarative_base()
 class Clients(Base_model):
     __tablename__ = "clients" 
 
-    id = Column(String, primary_key=True, index=True,default=lambda:str(uuid.uuid4()))
+    id = Column(String, primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
     first_name = Column(String, nullable=False)
     second_name = Column(String)
     account_created = Column(DateTime, default=datetime.utcnow, nullable=False)
     account_updated = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
-    admin = Column(Boolean, default=False)
 
-Base_model.metadata.create_all(bind=create_engine(LocalDatabase_URL))
+#Base_model.metadata.create_all(bind=create_engine(LocalDatabase_URL))
 
 Localsession = sessionmaker(autocommit=False, autoflush=False, bind=create_engine(LocalDatabase_URL))
 
@@ -73,18 +72,17 @@ async def Health_check(request: Request):
 
 
 @application.post("/Register", status_code=status.HTTP_201_CREATED)
-async def Client_Registration(First_Name: str, Second_Name: str, Email_id: str, password: str, Admin: bool):
+async def Client_Registration(First_Name: str, Second_Name: str, Email_id: str, password: str):
     DB = Localsession()
     Existing_client = DB.query(Clients).filter(Clients.email == Email_id).first()
     if Existing_client:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Client already exists")
-    New_client = Clients(email=Email_id, password=password, first_name=First_Name, second_name=Second_Name, admin=Admin)
+    New_client = Clients(email=Email_id, password=password, first_name=First_Name, second_name=Second_Name)
     DB.add(New_client)
     DB.commit()
     Client_Details={
         'Email':Email_id,
-        'Name':First_Name+' '+Second_Name,
-        'Admin':Admin
+        'Name':First_Name+' '+Second_Name
     }
     return {"Info" : "User registered Successfully","Client_Details": Client_Details}
 
@@ -102,20 +100,6 @@ async def Client_login(Email_id: str, password: str, db: Session = Depends(start
         return {"Message": "Logged in Successfully",'Client_details':Client_Loggedin_details}
     else:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Email id or password")
-
-@application.delete("/admin/users/Deletion", status_code=status.HTTP_204_NO_CONTENT)
-async def Delete_Client(Admin_Email_id: str, Admin_password: str,User_email_id:str, db: Session = Depends(start_db)):
-    Admin_existence_check=db.query(Clients).filter(Clients.email==Admin_Email_id,Clients.password==Admin_password).first()
-    if not Admin_existence_check:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="User Doesn't exists")
-    if not Admin_existence_check.admin:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Only admins can delete clients")
-    client_to_delete=db.query(Clients).filter(Clients.email==User_email_id).first()
-    if not client_to_delete:
-         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Client not found")
-    db.delete(client_to_delete)
-    db.commit()
-    return
 
 @application.put("/Client/update",status_code=status.HTTP_200_OK)
 async def Update_Client_Details(
