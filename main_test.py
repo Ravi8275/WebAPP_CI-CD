a4639d0976bafa407e22689dfd6d6bdb93580d16
Raviteja Inapala <inapala27@gmail.com>
2024-03-25 18:27:24 +0100

    add

--- main_test.py
+++ main_test.py
@@ -12,9 +12,9 @@ def test_client():
 
 
 def test_create_account():
-    response=requests.post('v1/user',json={'First_Name':'john','Second_Name':'Wick','Email_id':'Johnwick@gmail.com','password':'secret'})
+    response=requests.post('http://localhost:8000/v1/user',json={'First_Name':'john','Second_Name':'Wick','Email_id':'Johnwick@gmail.com','password':'secret'})
     assert response.status_code== status.HTTP_201_CREATED
 
 def test_update_account():
-    response=requests.put('/v1/user/{Email_id}',json={'First_Name':'john','Second_Name':'Wick','Email_id':'Johnwick@gmail.com','password':'secret','New_password':'no secret'})
+    response=requests.put('http://localhost:8000/v1/user/{Email_id}',json={'First_Name':'john','Second_Name':'Wick','Email_id':'Johnwick@gmail.com','password':'secret','New_password':'no secret'})
     assert response.status_code== status.HTTP_200_OK
